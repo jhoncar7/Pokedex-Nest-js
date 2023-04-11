dbb9747386a57ef3c77c208b65791b0e184154c1
Jhon Carreño <jhon7car@gmail.com>
2023-04-10 21:48:02 -0300

    union de la carpeta publica

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
+    PokemonModule],
 })
-export class AppModule {}
+export class AppModule { }
