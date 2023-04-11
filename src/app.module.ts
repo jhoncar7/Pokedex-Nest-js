803b590ddedb73490110b166a444ece903f6b015
Jhon Carreño <jhon7car@gmail.com>
2023-04-10 23:04:29 -0300

    conexion base de datos mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
   imports: [
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule],
 })
 export class AppModule { }
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { Pokemon, PokemonShema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports:[
+    MongooseModule.forFeature([
+      {
+        name:Pokemon.name,
+        schema:PokemonShema,
+      }
+    ]),
+  ]
 })
 export class PokemonModule {}
