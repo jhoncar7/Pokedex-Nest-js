308e6dd1b8dec1c84e43729d9a3e343086242ca3
Jhon Carreño <jhon7car@gmail.com>
2023-04-13 19:26:53 -0300

    validaciones en el modelo

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePokemonDto {}
+import { IsNumber, MinLength, IsPositive, IsString, Min } from "class-validator";
+
+export class CreatePokemonDto {
+
+    @IsNumber()
+    @Min(1)
+    @IsPositive()
+    no: number;
+
+    @MinLength(1)
+    @IsString()
+    name: string;
+
+}
